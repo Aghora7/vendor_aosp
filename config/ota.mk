04d80b8124e8e9267cd2f835de2cc00522cb5fc9
aghora7 <ukikieran@gmail.com>
2021-06-29 19:14:16 +0530

    Vendor: adding ota version code

--- config/ota.mk
+++ config/ota.mk
@@ -1,4 +1,12 @@
-ifneq ($(filter OFFICIAL CI,$(CUSTOM_BUILD_TYPE)),)
+ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
+
+LESSAOSP_OTA_VERSION_CODE := eleven
+
+PRODUCT_GENERIC_PROPERTIES += \
+    com.lessaosp.ota.version_code=$(LESSAOSP_OTA_VERSION_CODE) \
+    sys.ota.disable_uncrypt=1
+
 PRODUCT_PACKAGES += \
     Updates
+
 endif
