d0e6e225eb5e21fb81652890771671ece0bdc1f5
Aghora7 <ukikieran@gmail.com>
2022-01-09 03:22:30 +0530

    Vendor: Gapps path fixed

--- config/gapps.mk
+++ config/gapps.mk
@@ -1,6 +1,6 @@
 ifeq ($(WITH_GAPPS),true)
 # Gapps
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product, vendor/gapps/products/gapps.mk)
 
 # Common Overlay
 DEVICE_PACKAGE_OVERLAYS += \
